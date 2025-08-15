fe023e42873bd4f342c0091e51a2be0fd10c1808
Alixandria007 <juninhokrauts778@gmail.com>
2025-08-14 23:02:22 -0300

    Arrumando as dependencias

--- frontend_/app/notification.tsx
+++ frontend_/app/notification.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import {
-  SafeAreaView,
-} from 'react-native';
+import { SafeAreaView } from 'react-native';
 import HeaderWithOptions from '@/components/Layout/HeaderWithOptions';
 import Header from '@/components/Layout/Header';
 import NotificationSection from '@/components/NotificationSection';
 
 export default function NotificationScreen(): React.JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-        <Header avatarChar='A'/>
-        <HeaderWithOptions title='Notificações'/>
-        <NotificationSection/>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Header avatarChar="A" />
+      <HeaderWithOptions title="Notificações" />
+      <NotificationSection />
     </SafeAreaView>
   );
 }
