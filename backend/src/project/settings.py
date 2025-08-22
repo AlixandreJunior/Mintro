40ed4ae030a4f973e90bda9a34182ac907816f5f
AlixandreJunior <alixandreonetojunior@gmail.com>
2025-08-22 09:16:50 -0300

    Testes de estresse de objetives feitos.

--- backend/src/project/settings.py
+++ backend/src/project/settings.py
@@ -102,6 +102,7 @@ else:
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
+            "CONN_MAX_AGE": 0, 
         }
     }
 
